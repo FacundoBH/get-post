74a1c9da639a9cdffe96f2d97ed8267ab1db1c87
FacundoBH <102985869+FacundoBH@users.noreply.github.com>
2022-07-12 16:49:29 -0300

    Add files via upload

---
+++ recuperardatos.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = $_POST['nombre'];
+$dni = $_POST['dni'];
+$clave1 = $_POST['clave1'];
+//$clave2 = $_POST['clave2'];
+
+    
+    echo 'nombre: ' . $nombre . '<br>' . 'dni: ' . $dni . '<br>' . 'clave: ' . $clave1 . '<br>';
+  
+?>
